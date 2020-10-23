4edcaae8dd4c63f99b51b014455eb1a0a440efef
Bilgehan Gecici <bilgehangecici@gmail.com>
2020-10-23 23:03:41 +0300

    Update Description.md

---
+++ Smallest Multiple/smallestMult.js
@@ -0,0 +1,20 @@
+function smallestMult(number) {
+  var counter = 0;
+  var smallestNumber = [];
+
+  for (let index = 1; ; index++) {
+    for (let divider = 1; divider <= number; divider++) {
+      if (index % divider !== 0) {
+        break;
+      } else {
+        counter++;
+      }
+    }
+    if (counter === number) {
+      return index;
+    }
+    counter = 0;
+  }
+}
+
+console.log(smallestMult(10));
