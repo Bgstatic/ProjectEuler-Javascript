05feb12177a038467eb14009758a59763f3f94bd
Bgstatic <bilgehangecici@gmail.com>
2020-10-24 14:15:32 +0300

    Update Description.md

---
+++ 1001st Prime/nthPrime.js
@@ -0,0 +1,15 @@
+function nthPrime(n) {
+  var primeCounter = 0;
+  for (let index = 0; ; index++) {
+    if (isPrime(index)) primeCounter++;
+
+    if (primeCounter === n) return index;
+  }
+}
+
+function isPrime(num) {
+  for (var i = 2; i < num; i++) if (num % i === 0) return false;
+  return num > 1;
+}
+
+console.log(nthPrime(10001));
